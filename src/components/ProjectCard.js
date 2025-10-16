d499e82a3810e57ec7d1dadbd3682e45ec7959be
Bragadsss <david.braga@fatec.sp.gov.br>
2025-10-16 14:19:29 -0300

    Remove node_modules do controlo do Git

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,12 +6,7 @@ const ProjectCard = ({ project }) => {
       <img src={project.screenshot} alt={`Screenshot do ${project.name}`} className="project-image" />
       <div className="project-info">
         <h3>{project.name}</h3>
-        <p><strong>Descrição:</strong> {project.description}</p>
-        <p><strong>Tecnologias:</strong> {project.technologies}</p>
-        <p><strong>Minha Participação:</strong> {project.myRole}</p>
-        <a href={project.codeLink} target="_blank" rel="noopener noreferrer" className="project-link">
-          Ver Código no GitHub
-        </a>
+        <p>{project.description}</p>
       </div>
     </div>
   );
